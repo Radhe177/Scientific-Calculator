3dcc87d2e7e46dcbcf819b799f7eb2df002d2e7b
Radhe Sah <sahr22699@gmail.com>
2024-10-04 07:08:20 +0000

    implemented some scientific functionality

--- src/script.js
+++ src/script.js
@@ -1,36 +1,51 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     console.log("Document is ready");
     const display = document.getElementById('calc-display');
-    
+
     const buttons = document.getElementsByClassName('btn');
-       let currValue = "";
+    let currValue = "";
 
-       function evaluateResult(){
-          console.log('currentValue', currValue)
-          const convertedValue = currValue
-          .replace("×", "*")
-          .replace("÷", "/")
-          .replace("%", "*0.01");
-          console.log('convertedValue', convertedValue)
-          const result = eval(convertedValue);
-          currValue = result.toString();
-          display.value = currValue;
-       }
-    for(let i=0; i<buttons.length; i++) {
+    function evaluateResult() {
+        console.log('currentValue', currValue)
+        const convertedValue = currValue
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace("%", "*0.01")
+            .replace("sin", "Math.sin")
+            .replace("cos", "Math.cos")
+            .replace("ln", "Math.log")
+            .replace("π", "Math.PI")
+            .replace("log", "Math.log10")
+            .replace("e", Math.E)
+            .replace("tan", Math.tan)
+            .replace("√", Math.sqrt);
+        console.log('convertedValue', convertedValue)
+        const result = eval(convertedValue);
+        currValue = result.toString();
+        display.value = currValue;
+    }
+    for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
-        button.addEventListener("click", function(){
-           const value = button.innerText;
+        button.addEventListener("click", function () {
+            const value = button.innerText;
 
-          if(value == "AC") {
-            currValue = '';
-            display.value = currValue;
-          } else if(value == "=") {
-             evaluateResult();
-          }
-            else {
-            currValue += value;
-            display.value = currValue;
-          }
+            try {
+                if (value == "AC") {
+                    currValue = '';
+                    display.value = currValue;
+                } else if (value == "=") {
+                    evaluateResult();
+                }
+                else {
+                    currValue += value;
+                    display.value = currValue;
+                }
+            } catch (error) {
+                console.log(error);
+                currValue = "ERROR";
+                display.value = currValue;
+            }
+        }
 
            
         })
